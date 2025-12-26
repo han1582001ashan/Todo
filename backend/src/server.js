fbf5924c28f80bff11bca70aa76dd7aa035a7305
han1582001ashan <83277320+han1582001ashan@users.noreply.github.com>
2025-12-26 20:45:29 +0700

    ready deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,12 @@ import taskRouter from "./routes/taskRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 const app = express();
@@ -15,8 +18,18 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({origin:"http://localhost:5173"}))
+
 app.use("/api/tasks", taskRouter);
+
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({ origin: "http://localhost:5173" }));
+}
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(5001, () => {
